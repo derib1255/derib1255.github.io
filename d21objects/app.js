d964c82fabd9fea693d74c22928383f281e07ef8
derib1255 <mebaworku@gmail.com>
2021-09-08 00:37:03 -0500

    W2D1 updated

--- d21objects/app.js
+++ d21objects/app.js
@@ -1,7 +1,7 @@
 "use strict";
 
 /* You need the module.exports when testing in node.  Comment it out when you send your file to the browser */
-module.exports = { findTitles ,findAuthors,addBook,findIDs}; //add all of your function names here that you need for the node mocha tests
+module.exports = { findTitles, findAuthors, addBook, findIDs }; //add all of your function names here that you need for the node mocha tests
 
 let library = [
     { title: "The Road Ahead", author: "Bill Gates", libraryID: 1254 },
